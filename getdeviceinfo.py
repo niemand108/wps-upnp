8c3ad7b30abe92a94962af2a2a0aeb9d55aef412
niemand108 <santilozano@gmail.com>
2019-07-10 12:03:23 +0200

    two files

---
+++ getdeviceinfo.py
@@ -0,0 +1,37 @@
+import urllib2
+
+soap_encoding = "http://schemas.xmlsoap.org/soap/encoding/"
+soap_env = "http://schemas.xmlsoap.org/soap/envelope"
+service_ns = "urn:schemas-wifialliance-org:service:WFAWLANConfig:1"
+soap_body = """<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body>
+<u:GetDeviceInfo xmlns:u="urn:schemas-upnp-org:control-1-0">
+</u:GetDeviceInfo>
+</s:Body>
+</s:Envelope>
+"""
+
+
+#soap_body = """<soap-env:envelope soap-env:encodingstyle="%s" xmlns:soap-env="%s">
+#  <soap-env:body>
+#    <m:NewDiceInfo xmlns:m="%s">
+#    </m:NewDeviceInfo>
+#   </soap-env:body>
+#</soap-env:envelope>
+#""" % (soap_encoding, service_ns, soap_env)
+
+soap_action = "urn:schemas-wifialliance-org:service:WFAWLANConfig#GetDeviceInfo"
+headers = {
+    'SOAPAction': u'"%s"' % (soap_action),
+    'Host': u'192.168.0.1:1990',
+    'Content-Type': 'text/xml',
+    'Content-Length': len(soap_body),
+}
+
+ctrl_url = "http://192.168.0.1:1990/107fc877-460c-4b0d-91c2-48ff4e333181/control?WFAWLANConfig"
+
+request = urllib2.Request(ctrl_url, soap_body, headers)
+response = urllib2.urlopen(request)
+
+print response.read()
